e53e07384f63f2334bd8b847d7efbb48024bc955
Tanushri Singh <tanussingh@gmail.com>
2019-04-22 16:38:49 -0500

    Initial Commit

---
+++ Retrieval/retrieve.py
@@ -0,0 +1,15 @@
+import pymongo
+from pymongo import MongoClient
+import json
+
+def main():
+    # Set up connection to Mongo Server
+    client = MongoClient("localhost", 27017)
+
+    # Set connection to database
+    collection_Article = client[simple]
+
+    #
+
+if __name__ == "__main__"
+    main()
