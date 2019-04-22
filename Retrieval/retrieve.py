fefb012f627cdb715cf93cd0682ad1d2bfe30829
Tanushri Singh <tanussingh@gmail.com>
2019-04-22 16:52:45 -0500

    initial commit

--- Retrieval/retrieve.py
+++ Retrieval/retrieve.py
@@ -1,15 +1,22 @@
 import pymongo
 from pymongo import MongoClient
+import datetime
 import json
 
 def main():
+
+    start = datetime.datetime.strptime('2019-04-01', '%Y-%m-%d')
+    end = datetime.datetime.strptime('2019-04-01', '%Y-%m-%d')
+
     # Set up connection to Mongo Server
     client = MongoClient("localhost", 27017)
 
     # Set connection to database
-    collection_Article = client[simple]
+    collection_Article = client[articles]
 
-    #
+    # For ever file in every directory perform streaming
+    for doc in articles.file.find({'created_at': {'$gt': start, '$lt': end}}):
+        #Do whatever needs to be done
 
 if __name__ == "__main__"
     main()
