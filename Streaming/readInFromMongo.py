1c1ba2e7826b14690da1489a2d56116a6524fdb6
Mavis Francia <mavis.francia@gmail.com>
2019-05-04 14:43:53 -0500

    Publish story in proper JSON format

--- Streaming/readInFromMongo.py
+++ Streaming/readInFromMongo.py
@@ -2,12 +2,14 @@ import pymongo
 from pymongo import MongoClient
 from kafka import KafkaProducer
 from time import sleep
+from bson import ObjectId
 import json, sys, requests, time
+
 
 def publish_message(producer_instance, topic_name, value):
     try:
         key_bytes = bytes('foo', encoding='utf-8')
-        value_bytes = bytes(str(value), encoding='utf-8')
+        value_bytes = bytes(value, encoding='utf-8')
         producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
         producer_instance.flush()
         print('Message published successfully.')
@@ -15,11 +17,12 @@ def publish_message(producer_instance, topic_name, value):
         print('Exception in publishing message')
         print(str(ex))
 
+
 def connect_kafka_producer():
     _producer = None
     try:
         #_producer = KafkaProducer(value_serializer=lambda v:json.dumps(v).encode('utf-8'),bootstrap_servers=['localhost:9092'], api_version=(0, 10),linger_ms=10)
-         _producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10),linger_ms=10)
+         _producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10), linger_ms=10)
 
     except Exception as ex:
         print('Exception while connecting Kafka')
@@ -27,30 +30,41 @@ def connect_kafka_producer():
     finally:
         return _producer
 
+
+# for encoding ObjectId as string
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
 if __name__== "__main__":
     # Set up connection to Mongo Server
     client = MongoClient("localhost", 27017)
 
-    #Get user input on fileName
-    if len(sys.argv) != 4:
-        print ('Number of arguments is not correct')
-        exit()
+    # Get user input on fileName
+    # if len(sys.argv) != 4:
+    #     print ('Number of arguments is not correct')
+    #     exit()
 
-    #Read from mongo here
-    #assume that start and end date is in order 'Y-M-D'
-    fileName = sys.argv[1]
-    start = sys.argv[2]
-    end = sys.argv[3]
+    # Read from mongo here
+    # assume that start and end date is in order 'Y-M-D'
+    #  fileName = sys.argv[1]
+    #  start = sys.argv[2]
+    #  end = sys.argv[3]
     dbArticles = client['big_data']
 
     # Set connection to database
     collection_Article = dbArticles['spanish_articles']
 
-    #Write data onto Kafka
+    # Write data onto Kafka
     if (collection_Article.count()) > 0:
-        prod=connect_kafka_producer();
+        prod = connect_kafka_producer()
         for story in collection_Article.find():
+            story = json.dumps(story, cls=CustomJSONEncoder)
             print(story)
             publish_message(prod, 'SpanishArticles', story)
+            sleep(3)
         if prod is not None:
                 prod.close()
