6b68590c7cffd0d2530aaf494c100906603a9168
Mavis Francia <mavis.francia@gmail.com>
2019-05-04 17:28:52 -0500

    Add check for empty RDD

--- Streaming/streamToSpark.py
+++ Streaming/streamToSpark.py
@@ -16,11 +16,13 @@ sc = spark.sparkContext
 
 
 def do_something(rdd):
+    if rdd.count() == 0:
+        print('No data received from stream')
+        return
     json_rdd = rdd.map(lambda x: x[1])
     df = spark.read.json(json_rdd)
     udparse = udf(udpipe_parse, StringType())
     udparse_pos = udf(udpipe_pos, StringType())
-    print(df.columns)
     new_df = df.withColumn('udpipe_raw', udparse(df.text))
     new_df = new_df.withColumn('udpipe', udparse_pos(new_df.udpipe_raw))
     new_df.select('udpipe_raw', 'udpipe').show()
@@ -28,7 +30,7 @@ def do_something(rdd):
 
 if __name__ == "__main__":
     sc.setLogLevel("WARN")
-    ssc = StreamingContext(sc, 3)  # Batch duration set to 60secs
+    ssc = StreamingContext(sc, 30)  # Batch duration set to 60secs
 
     #Get topic name and broker information from user
     topic = 'SpanishArticles'
