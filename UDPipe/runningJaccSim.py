0518d9f23b1770247f7f91d202cd8de104aaa760
Mavis Francia <mavis.francia@gmail.com>
2019-05-07 20:26:05 -0500

    Read udparse from Mongo

--- UDPipe/runningJaccSim.py
+++ UDPipe/runningJaccSim.py
@@ -17,25 +17,31 @@ dbArticles = client['big_data']
 collection_d2v = dbArticles['d2v_calculated']
 d2v_values = []
 
+collection_ud = dbArticles['udpipe_parse']
+
 if (collection_d2v.count()) > 0:
     cursor = collection_d2v.find({"d2v_dup_count": {"$gt": 0}})
     df_d2v = pandas.DataFrame(list(cursor))
 
+if collection_ud.count() > 0:
+    cursor = collection_ud.find()
+    df_ud = pandas.DataFrame(list(cursor))
+
 #Get full list of spanish articles as pandas dataframe
 print(df_d2v.head(20))
-df_full = getDF()
+df_full = df_ud
 print(df_full)
 df_out = pandas.DataFrame(columns = ['url', 'SimilarityList'])
 
 for index, row in df_d2v.iterrows():
     url1 = row['url']
     text1 = df_full[df_full['url'] == url1].iloc[0]['text']
-    parse_pos = udpipe_pos(udpipe_parse(text1))
+    parse_pos = df_full[df_full['url'] == url1].iloc[0]['udpipe']
     parse_pos = json.loads(parse_pos)
     simList = {}
     for url2 in json.loads(row['d2v_sim']):
         text2 = df_full[df_full['url'] == url2].iloc[0]['text']
-        parse_pos2 = udpipe_pos(udpipe_parse(text2))
+        parse_pos2 = df_full[df_full['url'] == url2].iloc[0]['udpipe']
         parse_pos2 = json.loads(parse_pos2)
         s1 = set(parse_pos['AUX']) | set(parse_pos['NUM']) | set(parse_pos['VERB']) | set(parse_pos['NOUN']) | set(parse_pos['PROPN'])
         s2 = set(parse_pos2['AUX']) | set(parse_pos2['NUM']) | set(parse_pos2['VERB']) | set(parse_pos2['NOUN']) | set(parse_pos2['PROPN'])
